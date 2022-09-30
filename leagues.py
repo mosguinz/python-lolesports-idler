a98f54fd35a815cd7920a199faba8c9aa62226bc
mosguinz <mos.guinz@gmail.com>
2022-09-30 12:34:41 -0700

    Add leagues

---
+++ leagues.py
@@ -0,0 +1,8 @@
+LEAGUES = ['worlds', 'lcs', 'lcs-academy', 'proving_grounds', 'lcs_amateur', 'college_championship',
+           'turkey-academy-league', 'cblol-brazil', 'lck', 'lcl', 'lco', 'lec', 'ljl-japan', 'lla', 'lpl', 'pcs',
+           'turkiye-sampiyonluk-ligi', 'vcs', 'all-star', 'european-masters', 'lfl', 'nlc', 'elite_series',
+           'liga_portuguesa', 'pg_nationals', 'ultraliga', 'superliga', 'primeleague', 'hitpoint_masters',
+           'esports_balkan_league', 'greek_legends', 'lck_academy', 'ljl_academy', 'lck_challengers_league',
+           'cblol_academy', 'master_flow_league', 'movistar_fiber_golden_league', 'elements_league',
+           'claro_gaming_stars_league', 'honor_division', 'volcano_discover_league', 'honor_league', 'msi',
+           'tft_esports']
